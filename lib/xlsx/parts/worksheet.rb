cc275b826116d2aee60e34983a001f848aaae2c1
Bob Lail <bob.lail@cph.org>
2014-10-11 16:45:16 -0500

    [improvement] Supported column widths (10m)

--- lib/xlsx/parts/worksheet.rb
+++ lib/xlsx/parts/worksheet.rb
@@ -7,6 +7,12 @@ module Xlsx
         @workbook = workbook
         @index = index
         @rows = []
+        @column_widths = {}
+      end
+
+      def column_widths(*args)
+        return @column_widths if args.none?
+        @column_widths = args.first
       end
 
       def add_rows(*rows)
@@ -21,17 +27,22 @@ module Xlsx
 
       def to_xml
         build_standalone_xml do |xml|
-          xml.worksheet(root_namespaces) {
+          xml.worksheet(root_namespaces) do
             xml.sheetViews do
               xml.sheetView(showGridLines: 0, tabSelected: 1, workbookViewId: 0)
             end
             xml.sheetFormatPr(baseColWidth: 10, defaultColWidth: 13.33203125, defaultRowHeight: 20, customHeight: 1)
-            xml.sheetData {
+            xml.cols do
+              column_widths.each do |column, width|
+                xml.col(min: column, max: column, width: width, customWidth: 1)
+              end
+            end if column_widths.any?
+            xml.sheetData do
               rows.each { |row| row.to_xml(xml) }
-            }
+            end
             xml.pageMargins(left: 0.75, right: 0.75, top: 1, bottom: 1, header: 0.5, footer: 0.5)
             xml.pageSetup(orientation: "portrait", horizontalDpi: 4294967292, verticalDpi: 4294967292)
-          }
+          end
         end
       end
 
