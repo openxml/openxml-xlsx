c43a08ead86df2174fd9750c6c398d35ddcbf9d5
Luke Booth and Ordie Page <pair=luke.booth+ordie.page@cphepdev.com>
2015-05-08 14:07:47 -0500

    [skip] Added the hidden property to rows (5m)

--- lib/xlsx/elements/row.rb
+++ lib/xlsx/elements/row.rb
@@ -1,12 +1,13 @@
 module Xlsx
   module Elements
     class Row
-      attr_reader :worksheet, :number, :height, :cells
+      attr_reader :worksheet, :number, :height, :hidden, :cells
       
       def initialize(worksheet, options={})
         @worksheet = worksheet
         @number = options.fetch(:number)
         @height = options[:height]
+        @hidden = options[:hidden]
         @cells = []
         
         Array(options[:cells]).each do |attributes|
@@ -21,6 +22,7 @@ module Xlsx
       def to_xml(xml)
         attributes = {"r" => number}
         attributes.merge!("ht" => height, "customHeight" => 1) if height
+        attributes.merge!("hidden" => 1) if hidden
         xml.row(attributes) do
           cells.each do |cell|
             cell.to_xml(xml)
